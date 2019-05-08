c2d4b6b761c55ff8ad2c5059bbadb075ef6cdc30
karan verma <karan.verma@zoomcar.com>
2019-05-08 20:41:51 +0530

    feat: updating resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,18 +1,22 @@
 import {getBooks, 
         getBooksByAuthor, 
         getBooksByTitle,
-        addBook
+        addBook,
+        getAuthors
       } from '../connectors'
 
 export default {
   Query: {
     books(_, args, ctx) {
-    return getBooks(args.filter, args.val)
+    return getBooks(args.filter, args.val);
     },
+    authors(_, args, ctx) {
+      return getAuthors();
+    }
   },
   Mutation: {
     addBook(_, args, ctx) {
-      return addBook(args)
+      return addBook(args);
     }
   }
 };
